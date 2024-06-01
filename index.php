25108fc51d3aab2e6351fe0963c695534475de33
theCirillio <kirillbazhenovcool@gmail.com>
2024-06-02 02:11:59 +0700

    quest2 - functions

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>my js</title>
+    <title>My_JS</title>
+    <link rel="icon" type="image/x-icon" href="/pics/javascript-svgrepo-com.svg">
     <link rel="stylesheet" type="text/css" href="/css/index.css">
 </head>
 
@@ -17,20 +18,36 @@
             <input type="submit" value="Рассчитать">
         </form>
         <input readonly type="text" id="oneday">
-        <div id="months" style="display: none; flex-direction:column; gap: 20px;">
+        <input readonly type="text" id="moneystatus">
+        <div id="months" style="display: flex; flex-direction:column; gap: 20px;">
             <form action="">
-                <input type="text" name="month" placeholder="Статья расходов в этом месяце?">
-                <input type="text" name="money" placeholder="Во сколько обойдется?">
+                <input type="text" name="month" placeholder="Статья расходов в этом месяце?" value="">
+                <input type="text" name="money" placeholder="Во сколько обойдется?" value="">
                 <input type="submit" value="Добавить">
             </form>
             <form action="">
-                <input type="text" name="month" placeholder="Статья расходов в следующем месяце?">
-                <input type="text" name="money" placeholder="Во сколько обойдется?">
+                <input type="text" name="month" placeholder="Статья расходов в следующем месяце?" value="">
+                <input type="text" name="money" placeholder="Во сколько обойдется?" value="">
                 <input type="submit" value="Добавить">
             </form>
         </div>
+        <div id="savings" style="display: none; flex-direction:column; gap:20px; align-items:center;">
+            <p>У вас не найдено накоплений!</p>
+            <p>Хотите открыть?</p>
+            <div id="buttons" style="display: flex; flex-direction:column; width:100px; gap: 10px;">
+                <button class="choose" value="1">Да</button>
+                <button class="choose" value="0">Нет</button>
+            </div>
+            <div id="openSavings" style="display: none;flex-direction:column; gap:20px;">
+                <form action="" id="addSaving">
+                    <input type="text" placeholder="Какова сумма накоплений?" name="sum" value="" id="savingSum">
+                    <input type="text" placeholder="Под какой процент?" name="percent" value="" id="savingPercent">
+                    <input type="submit" value="Открыть">
+                </form>
+                <input type="text" readonly id="openedSaving" style="display: none;">
+            </div>
+        </div>
     </div>
-
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="/js/script_index.js"></script>
--- js/script_index.js
+++ js/script_index.js
@@ -1,56 +1,118 @@
-'use strict';
+"use strict";
 
-class UserBudget{
-    constructor(money, time){
-        if(typeof money !== 'number' || typeof time !== 'string'){
-            throw new TypeError('Both arguments must be of type number and string');
-        }
-        this.money = money;
-        this.time = time;
+class UserBudget {
+  constructor(money, time) {
+    if (typeof money !== "number" || typeof time !== "string") {
+      // TYPEOF
+      throw new TypeError("Both arguments must be of type number and string");
     }
+    this.money = money;
+    this.time = time;
+  }
 }
 
-
 let appData = {
-    budget: null,
-    timeData: null,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: false
+  budget: null,
+  timeData: null,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false,
 };
 
+$(document).ready(function () {
+  $("#budget").on("submit", function (e) {
+    e.preventDefault();
+    const money = parseFloat($("#money").val()); // PARSEFLOAT
+    const time = $("#time").val();
 
-$(document).ready(function() {
-    $("#budget").on("submit", function(e) {
-        e.preventDefault();
-        const money = parseFloat($("#money").val());
-        const time = $("#time").val();
+    if (isNaN(money)) {
+      throw new Error("Money must be a number");
+    }
 
-        if (isNaN(money)) {
-            throw new Error('Money must be a number');
-        }
+    const userBudget = new UserBudget(money, time);
+    console.log(userBudget);
+    appData.budget = userBudget.money;
+    appData.timeData = userBudget.time;
+    console.log(appData);
+    $("#money").val("");
+    $("#time").val("");
+    appData.perDay = parseFloat((money / 30).toFixed(2)); // tofixed - returns STRING, parse Float for float type
+    $("#oneday").val(`Ваш бюджет на 1 день: ${appData.perDay}`);
+    if (perDay < 100) {
+      $("#moneystatus").val(`Ваш достаток беден.`);
+    } else if (perDay > 100 && perDay < 2000) {
+      $("#moneystatus").val(`Ваш достаток средний.`);
+    } else if (perDay > 2000) {
+      $("#moneystatus").val(`Ваш достаток богат.`);
+    } else {
+      alert("Произошла непредвиденная ошибка.");
+    }
+  });
 
-        const userBudget = new UserBudget(money, time);
-        console.log(userBudget);
-        appData.budget = userBudget.money;
-        appData.timeData = userBudget.time;
-        console.log(appData);
-        $("#oneday").val(`Ваш бюджет на 1 день: ${money/30}`);
-        $("#months").css("display", "flex");
+  $("#months").on("submit", "form", function (e) {
+    e.preventDefault();
+    const month = $(this).find("input[name=month]").val();
+    const money = parseFloat($(this).find("input[name=money]").val());
+    if (!isNaN(money)) {
+      appData.expenses[month] = money; // ADD TO OBJECT
+      console.log(appData.expenses);
+    } else {
+      $(this).find("input[name=month]").val("");
+      $(this).find("input[name=money]").val("");
+      alert("Введены некорректные данные или они отсутствуют");
+    }
+  });
+
+  if (appData.savings == 0) {
+    // ЕСЛИ СОХРАНЕНИЙ НЕТ ТО ПРЕДЛОЖИТЬ ДОБАВИТЬ ИХ
+    console.log(appData.savings);
+    $("#savings").css({
+      display: "flex", // ОТКРЫТЬ ЗАПРОС НА ДОБАВЛЕНИЕ
     });
+  }
+  $(".choose").click(function () {
+    if ($(this).val() == 0) {
+      // ПРОВЕРКА ЗНАЧЕНИЯ НАЖАТОЙ КНОПКИ
+      $("#savings").css("display", "none");
+    } else {
+      appData.savings = true;
+      console.log(appData.savings);
+      $("#openSavings").css({
+        display: "flex", // ОТКРЫТЬ ДОБАВЛЕНИЕ СОХРАНЕНИЙ
+      });
+      $("#buttons").css("display", "none"); // УБРАТЬ КНОПКИ ЧТОБ НЕ МЕШАЛИСЬ
+    }
+  });
 
-    $("#months").on("submit", "form", function(e) {
-        e.preventDefault();
-        const month = $(this).find("input[name=month]").val();
-        const money = parseFloat($(this).find("input[name=money]").val());
-
-        if (isNaN(money)) {
-            throw new Error('Money must be a number');
-        }
-
-        appData.expenses[month] = money;
-        console.log(appData.expenses);
+  $("#addSaving").submit(function (e) {
+    e.preventDefault();
+    const savingData = $(this).serializeArray(); // ПОЛУЧЕНИЕ ДАННЫХ В ВИДЕ МАССИВА ARRAY[INDEX].VALUE/NAME
+    console.log(savingData);
+    $.each(savingData, function (index, value) {
+      console.log(value.name + ": " + value.value);
     });
-})
-
+    const sum = parseFloat(savingData[0].value);
+    const percent = parseFloat(savingData[1].value);
+    if (
+      sum === null ||
+      percent === null ||
+      sum <= 0 ||
+      percent <= 0 ||
+      percent >= 100 ||
+      isNaN(sum) ||
+      isNaN(percent)
+    ) {
+      $("#addSaving").get(0).reset("input");
+      console.log("ERROR in #" + $("#addSaving").attr("id"));
+      alert("Неправильно введены данные. Перепроверьте для заполнения.");
+    } else {
+      appData.monthInCome = ((sum / 100 / 12) * percent).toFixed(2);
+      console.log(appData.monthInCome);
+      $("#openedSaving").css({
+        display: "flex",
+      });
+      $("#openedSaving").val("Доход в месяц: " + appData.monthInCome);
+    }
+  });
+});
